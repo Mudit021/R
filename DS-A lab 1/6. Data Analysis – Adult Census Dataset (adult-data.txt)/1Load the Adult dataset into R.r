cebe9af8cbaf0f7a0437f81c644b918460f26531
Mudit021 <mudit00022@gmail.com>
2025-08-19 10:08:03 +0530

    lab 1 changes

--- DS-A lab 1/6. Data Analysis – Adult Census Dataset (adult-data.txt)/1Load the Adult dataset into R.r
+++ DS-A lab 1/6. Data Analysis – Adult Census Dataset (adult-data.txt)/1Load the Adult dataset into R.r
@@ -1,6 +1,6 @@
 # import the dataset
 data1 <- read.table("DS-A lab 1\\6. Data Analysis – Adult Census Dataset (adult-data.txt)\\adult-data.txt")
-colnames(data1) <- c('Age','Job-Type','Social Security Number','Education','Years','Marital Status','Job','Family','Color','Sex','','','','Country','Salary')
+colnames(data1) <- c('Age','Job-Type','Social Security Number','Education','Years','Marital Status','Job','Family','Color','Sex','Capital_gain','Capital_Loss','Hour_Per_Week','Country','Salary')
 
 # View the dataset
 View(data1)
--- DS-A lab 1/6. Data Analysis – Adult Census Dataset (adult-data.txt)/2Display the first 10 rows.r
+++ DS-A lab 1/6. Data Analysis – Adult Census Dataset (adult-data.txt)/2Display the first 10 rows.r
@@ -1,5 +1,5 @@
 data1 <- read.table("DS-A lab 1\\6. Data Analysis – Adult Census Dataset (adult-data.txt)\\adult-data.txt")
-colnames(data1) <- c('Age','Job-Type','Social Security Number','Education','Years','Marital Status','Job','Family','Color','Sex','','','','Country','Salary')
+colnames(data1) <- c('Age','Job-Type','Social Security Number','Education','Years','Marital Status','Job','Family','Color','Sex','Capital_gain','Capital_Loss','Hour_Per_Week','Country','Salary')
 
 #display the first 10 data from the dataset
 head(data1,10)

be6296041e4542fae891575a5d5e882eb5a167a2
Mudit021 <mudit00022@gmail.com>
2025-08-19 10:05:45 +0530

    lab1

--- DS-A lab 1/6. Data Analysis – Adult Census Dataset (adult-data.txt)/2Display the first 10 rows.r
+++ DS-A lab 1/6. Data Analysis – Adult Census Dataset (adult-data.txt)/2Display the first 10 rows.r
@@ -1,6 +1,5 @@
-# import the dataset
 data1 <- read.table("DS-A lab 1\\6. Data Analysis – Adult Census Dataset (adult-data.txt)\\adult-data.txt")
 colnames(data1) <- c('Age','Job-Type','Social Security Number','Education','Years','Marital Status','Job','Family','Color','Sex','','','','Country','Salary')
 
-# View the dataset
-View(data1)
+#display the first 10 data from the dataset
+head(data1,10)
